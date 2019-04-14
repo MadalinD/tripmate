1fe7bee2efe50d08fcc20c7fa51dc3d237eb2416
AshGale <ashgale17@yahoo.co.uk>
2019-04-14 18:31:27 +0100

    Added some basic test endpoints to play with

--- src/main/java/co/uk/mak/tripmate/WelcomeController.java
+++ src/main/java/co/uk/mak/tripmate/WelcomeController.java
@@ -1,9 +1,13 @@
 package co.uk.mak.tripmate;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+//@RequestMapping("/")
 public class WelcomeController {
 
 	//this loads the index page from temples when / is called
@@ -12,4 +16,18 @@ public class WelcomeController {
 		return "index";
 	}
 	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping(path= "/json", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<String> json() {
+		return ResponseEntity.ok("{\"test\":\"data\"}");
+	}
+	
+	@GetMapping(path= "/test", produces = "application/json")
+	public ResponseEntity<String> test() {
+		return ResponseEntity.ok("test");
+	}
 }
